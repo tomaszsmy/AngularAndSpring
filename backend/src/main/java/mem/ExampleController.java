a31784e9d3cc259e49d6cf8fad86a7f50ebcc231
tom-smy457 <tomaszmyczynski@gmail.com>
2022-01-16 19:12:28 +0100

    Initial commit

---
+++ backend/src/main/java/mem/ExampleController.java
@@ -0,0 +1,13 @@
+package mem;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+    @GetMapping("/example")
+    public String example() {
+        return "example";
+    }
+}
---
+++ backend/src/main/java/mem/MemApplication.java
@@ -0,0 +1,13 @@
+package mem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemApplication.class, args);
+	}
+
+}
